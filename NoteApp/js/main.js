f82eb18d692241b80ee6c612dcc785d1be8f80a5
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2021-12-31 12:35:07 +0530

    Add files via upload

---
+++ NoteApp/js/main.js
@@ -0,0 +1,113 @@
+console.log("Hello guys i'm  today we gonna make a NOTE APP by RAJ NIRALA.");
+showNotes();
+let containor = document.getElementById("containor");
+let addbtn = document.getElementById("addBtn");
+// let delbtn = document.getElementsByClassName("delBtn");
+
+
+
+addbtn.addEventListener("click", function (e) {
+    let text = document.getElementById("addText");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesList = [];
+    }
+    else {
+        notesList = JSON.parse(notes)
+    }
+    notesList.push(text.value);
+    addnote(text.value, notesList.length);
+    // showNotes()
+    localStorage.setItem("notes", JSON.stringify(notesList));
+    text.value = "";
+    // console.log(notesList);
+});
+
+//show note function
+function showNotes() {
+    // console.log("hello guys i'm raj nirala");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesList = [];
+    }
+    else {
+        notesList = JSON.parse(notes)
+    }
+    // console.log(notesList);
+    let card = "";
+    notesList.forEach((noteText, index) => {
+        card += `<div class="card my-3 mx-3 note" style="width: 18rem;">
+        <div class="card-body">
+     <h5 id='noteTitle' class="card-title">Note ${index + 1}</h5>
+     <p class="card-text">${noteText}</p>
+     </div>
+     <button type="button" id="${index}" onclick="delnote(this.id)"
+     class="btn  btn-dark">Delete</button>
+     </div>
+     </div>`;
+    });
+    let showhere = document.getElementById("shownoteshere");
+    showhere.innerHTML = card;
+    
+}
+
+//add note function
+function addnote(note, index) {
+    let showhere = document.getElementById("shownoteshere");
+    // let noteCard = document.getElementById("note");
+    showhere.innerHTML += `<div class="card my-3 mx-3 note" style="width: 18rem;">
+    <div class="card-body">
+    <h5 id='noteTitle' class="card-title">Note ${index}</h5>
+    <p class="card-text">${note}</p>
+    </div>
+    <button type="button" id="${index}" onclick="delnote(this.id)"
+     class="btn  btn-dark">Delete</button>
+     </div>
+     </div>`;
+    };
+    
+    
+    //note delete function
+    function delnote(index){
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesList = [];
+    }
+    else {
+        notesList = JSON.parse(notes);
+    }
+    notesList.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesList));
+    showNotes();
+    
+}
+
+
+//search bar
+let search = document.getElementById("searchtext");
+search.addEventListener("input", function () {
+    let searchValue = search.value;
+    let searchValueCapi = searchValue.toUpperCase()
+    // console.log(searchValueCapi);
+    let notes = document.getElementsByClassName("note");
+    Array.from(notes).forEach(function (element){
+        // console.log(element);
+        let card = element.getElementsByTagName("p")[0].innerText;
+        if (card.includes(searchValue)) {
+            element.style.display = "block";
+        }else if(card.includes(searchValueCapi)) {
+            element.style.display = "block";
+        }
+        else {
+            element.style.display = "none";
+        }
+    });
+});
+
+//during null div
+let showhere = document.getElementById("shownoteshere");
+if (showhere.innerHTML == []) {
+    showhere.innerHTML = `<h4 style="color:red;">Nothing to show here! use "Add Note" to add notes.<h4>`;
+} else {
+    showNotes();
+}
