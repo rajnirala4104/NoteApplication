8bb8171260c5ea0de0d236a3ab2e3fbf885c558a
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-14 21:43:45 +0530

    node js added

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req,res)=>{
+    res.status(200).render('index')
+})
+
+module.exports = app;
