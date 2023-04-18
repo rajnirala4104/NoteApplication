1ea003669e4f502727b3344610ad8707e0dc9a64
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-18 21:08:12 +0530

    littile changes in serverjs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const {createServer} = require('http')
 const app = require('./app')
 
 const port = 5000
-createServer(app).listen(port, (err, data)=>{
+const server = createServer(app)
+
+
+server.listen(port, (err, data)=>{
     console.log(`Server has started at http://127.0.0.1:${port}`)
 })
